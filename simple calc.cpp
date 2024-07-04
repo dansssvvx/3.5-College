6deea35d5fd97aaeb2bda0fd4a9256d689f57c95
Ramaa <55214992+dansssvvx@users.noreply.github.com>
2024-07-04 10:45:42 +0800

    Add files via upload

---
+++ simple calc.cpp
@@ -0,0 +1,60 @@
+// simple calculator implementation on c++
+
+#include <iostream>
+using namespace std;
+
+struct calc{
+    int num, num2;
+    int result;
+    char operation;
+}; 
+
+int function(int a, int b, char d){
+    int c;
+
+    if ( d == '+'){
+        c = a + b;
+    } else if ( d == '-' ){
+         c = a - b;
+    } else if ( d == '/' ){
+        c = a / b;    
+    } else if (d == '*' ){
+        c = a * b;
+    } else{
+        cout << "operation error!"
+             << "please input valid operatoin"
+             << "(- + / *)";
+    }
+    
+    cout << c;
+
+    return c;
+} 
+
+int main(){
+	int pilihan, num, num1;
+	char op;    
+    cout << "choose function\n"
+         << "1. calculator\n"
+         << "2. exit\n"
+         << ">> "; cin >> pilihan;
+    
+    if (pilihan == 1 || pilihan == 2){
+    	if (pilihan != 2){
+    		cout << "pls input the number\n"
+    			 << "syntax, (number, operation, number)\n"
+    			 << "ex : 4+5\n"
+    			 << " >> "; cin >> num; cin >> op; cin >> num1;
+    			
+    		function (num, num1, op);
+    	} else {
+    		return 0;
+		}
+	} else {
+		cout << "wrong number to input\n"
+			 << "pls input the right numb\n";
+	}
+         
+    
+
+}
